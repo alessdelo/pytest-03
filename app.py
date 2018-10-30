17178e80679d91c8c40619d9cdfa69ca9481f710
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-10-30 17:50:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 port = os.environ.get('PORT', 5000)
-    # Run the app.
-    run(host='0.0.0.0', port=port)  
+# Run the app.
+run(host='0.0.0.0', port=port)  
 print(dir(os))
 # from bottle import route, run
 # port = int(os.environ.get('PORT', 17995))
