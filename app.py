ea61df504bd30b18fa5bcd477c934f1c654ac388
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-10-30 17:48:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-
-"""
 import os
-from bottle import route, run
-
-@route('/hello/:name')
-def index(name='World'):
-    return '<b>Hello %s!</b>' % name
-
-
-if __name__ == '__main__':
-    # Get required port, default to 5000.
-    port = os.environ.get('PORT', 5000)
-
-    # Run the app.
-    run(host='0.0.0.0', port=port)
-""""
-
-import os
-
 port = os.environ.get('PORT', 5000)
     # Run the app.
-    run(host='0.0.0.0', port=port)
-    
+    run(host='0.0.0.0', port=port)  
 print(dir(os))
-
 # from bottle import route, run
 # port = int(os.environ.get('PORT', 17995))
-
 a=1+1
 b=2+2
 a+b
