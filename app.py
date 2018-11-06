4b762972b8fa0c9971ec6262996565c95c3b2153
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-06 22:40:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ from bottle import route, run
 def index(name='World'):
     return '<b>Hello %s!</b>' % name
 
+@route('/page1')
+def page1():
+    mystring = '''<html>
+    <head><title>Page 1</title></head>
+    <body>'''
+    mystring += '<p>Bla, Bla, Bla.....</p>'
+    mystring += '</body></html>'    
+    return mystring
+
 if __name__ == '__main__':
     # Get required port, default to 5000.
     port = os.environ.get('PORT', 5000)
