5bb27e2edd85f92c84203b9a7bb35d4158576218
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-06 14:37:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
-port = os.environ.get('PORT', 5000)
-# Run the app.
 from bottle import route, run
-run(host='0.0.0.0', port=port)
-# port = int(os.environ.get('PORT', 17995))
-print(dir(os))
-a=1+1
-b=2+2
-a+b
-print ('''<html>
-<head><title>My first Python CGI app</title></head>
-<body>
-<p>Hello, 'world'!</p>
-</body>
-</html>''')
+
+@route('/hello/:name')
+def index(name='World'):
+    return '<b>Hello %s!</b>' % name
+
+
+if __name__ == '__main__':
+    # Get required port, default to 5000.
+    port = os.environ.get('PORT', 5000)
+
+    # Run the app.
+    run(host='0.0.0.0', port=port)
