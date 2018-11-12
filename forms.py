45f8257a19753ddee5111ee70b171273c2cb4897
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-12 22:03:43 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,10 +2,11 @@
 from flask_wtf import FlaskForm
 # imports classes of various fields 
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password'), validators[DataRequired(), ]
+    password = PasswordField('Password', validators[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators[DataRequired(), EqualTo('password')])
   
