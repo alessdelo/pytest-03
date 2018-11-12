5526f11643d152eca92c9b54a6cf3be28b43ef07
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-12 21:59:24 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 # imports FlaskForm class that automatically generates an html form
 from flask_wtf import FlaskForm
 # imports classes of various fields 
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForm(FaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password'), validators[DataRequired(), ]
   
