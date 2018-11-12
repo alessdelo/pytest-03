ea0f3504dfe35d666541066f012373a22541e450
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-12 21:57:21 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,8 +2,9 @@
 from flask_wtf import FlaskForm
 # imports classes of various fields 
 from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForm(FaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
   
