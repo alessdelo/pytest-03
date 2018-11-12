e3e160326c72236925cd31187f990ab206e6fbaf
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-12 21:45:38 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,7 @@
+# imports FlaskForm class that automatically generates an html form
 from flask_wtf import FlaskForm
+# imports classes of various fields 
+from wtforms import StringField
 
 class RegistrationForm(FaskForm):
   
