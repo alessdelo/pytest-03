9282ff4f175c8c47f637a6e03900f1525ad3cc0b
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-13 14:47:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from forms import RegistrationForm, LoginForm
 # creates an instance of Flask class
 app = Flask(__name__)
 
-# app.config('SECRET_KEY') = '5791628bb0b13ce1c676dfde280ba245'
+app.config('SECRET_KEY') = '5791628bb0b13ce0c676dfde280ba245'
 
 # some dummy data...
 posts = [
