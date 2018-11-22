42b751c0e2515e618d14faaaacae689c2d80cbbb
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-11-22 13:59:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 #on heroku config vars are setted in settings/config vars
-# app.config('SECRET_KEY') = '5791628bb0b13ce0c676dfde280ba245'
+app.config('SECRET_KEY') = '5791628bb0b13ce0c676dfde280ba245'
 
 # some dummy data...
 posts = [
