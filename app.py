2a6bd79c5fa0fac72e598f227918f89757073dfc
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-12-13 07:12:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About', he_keys=[secret_key, csrf_secret_key])
+    return render_template('about.html', title='About', the_keys=[secret_key, csrf_secret_key])
 
 # ------------------------------------
 
