96abbbf52a9bab33b136e7340c439fc4d867b364
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-12-21 06:52:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ CSRFProtect(app)
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
 
- SECRET_KEY = os.environ['SECRET_KEY']
- WTF_CSRF_SECRET_KEY = os.environ['WTF_CSRF_SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
+WTF_CSRF_SECRET_KEY = os.environ['WTF_CSRF_SECRET_KEY']
         
 # some dummy data...
 posts = [
